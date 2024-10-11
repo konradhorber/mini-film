847bf0e83a3bb4453a3bdca0b6c630f6019b15e7
konradhorber <konrad.horber@gmail.com>
2024-10-11 09:35:07 +0100

    Add finding film by ID

--- routes/films.js
+++ routes/films.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const Film = require("../models/Film");
 
 router.get("/", async (req,res) => {
-    
     try{
         const films = await Film.find();
         res.send(films)
@@ -12,5 +11,14 @@ router.get("/", async (req,res) => {
         res.send({message:err});
     }
 });
+
+router.get("/:filmId", async(req,res) => {
+    try{
+        const filmById = await Film.findById(req.params.filmId);
+        res.send(filmById)
+    }catch(err){
+        res.send({message:err});
+    }
+})
 
 module.exports = router;
